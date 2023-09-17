28360ead34b96fff8678c4caf3ded187039bff97
Barbie Joy Obas <barbiejoy.obas@students.isatu.edu.ph>
2023-09-17 13:20:37 +0000

    initial commit

---
+++ act__1.R
@@ -0,0 +1,17 @@
+#Define n
+n <- 100
+
+#Calculate the sum using the formula (n *(n + 1)) / 2
+sum_int <- (n *(n + 1)) / 2
+
+# Print the result
+print(sum_int)
+
+#Define n
+n <- 1000
+
+#Calculate the sum using the formula (n *(n + 1)) / 2
+sum_int <- (n *(n + 1)) / 2
+
+# Print the result
+print(sum_int)
